09a63fac38141307962c1745614147fa8e9b29e7
lukamagic02 <lr53754@fer.hr>
2023-11-01 20:21:18 +0100

    dodan osnovni kostur backend-a

---
+++ IzvorniKod/backend/src/main/java/com/lostpetfinder/LostPetFinderApplication.java
@@ -0,0 +1,13 @@
+package com.lostpetfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostPetFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostPetFinderApplication.class, args);
+	}
+
+}
