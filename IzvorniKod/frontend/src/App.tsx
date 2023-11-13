01f2122444e083be7dc92c33cf3aadc241e8e424
0xDontCare <hogar1.veliki@gmail.com>
2023-11-13 23:29:12 +0100

    Revert "Merge branch 'dev' of https://github.com/PatrikMFer/A-Team into dev"
    This reverts commit 3b39ab61e1a5f4d4fed14fd855b2aac756d6153c, reversing
    changes made to 7b8fa95de63d88b0d295b18e5e5a4892f2e31b92.

--- IzvorniKod/frontend/src/App.tsx
+++ IzvorniKod/frontend/src/App.tsx
@@ -1,62 +1,41 @@
-import {useEffect, useState} from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import NavbarElement from "./components/NavbarElement";
 import OglasDetalj from "./components/OglasDetalj.tsx";
 import AddAd from "./components/AddAd";
-import axios from "axios";
 
 function App() {
-    const [isLoggedIn, setLoginStatus] = useState(false); // State to track login status
-    const [userData, setUserData] = useState(null); // State to store user data
+  const [isLoggedIn, setLoginStatus] = useState(false); // State to track login status
+  const cardData = [
+    { id: 1, title: "Oglas 1", content: "This is some content for Card 1." },
+    { id: 2, title: "Oglas 2", content: "This is some content for Card 2." },
+    { id: 3, title: "Oglas 3", content: "This is some content for Card 3." },
+    { id: 4, title: "Oglas 4", content: "This is some content for Card 4." },
+  ];
 
-    // Function to update user data after a successful login
-    useEffect(() => {
-        if (isLoggedIn) {
-            axios.get("/api/logged") // Replace with your API endpoint
-                .then((response) => {
-                    setUserData(response.data);
-                })
-                .catch((error) => {
-                    console.error("Error fetching user data: ", error);
-                });
-        }
-    }, [isLoggedIn]);
-
-    const updateUser = () => {
-        setLoginStatus(true);
-    };
-
-    const cardData = [
-        {id: 1, title: "Oglas 1", content: "This is some content for Card 1."},
-        {id: 2, title: "Oglas 2", content: "This is some content for Card 2."},
-        {id: 3, title: "Oglas 3", content: "This is some content for Card 3."},
-        {id: 4, title: "Oglas 4", content: "This is some content for Card 4."},
-    ];
-
-    return (
-        <div>
-            <NavbarElement isLoggedIn={isLoggedIn} userData={userData}/>
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<Home cardData={cardData} isLoggedIn={isLoggedIn}/>}
-                    />
-                    <Route path="/register" element={<Register/>}/>
-                    <Route
-                        path="/login"
-                        element={<Login setLoginStatus={updateUser} setUserData={setUserData}/>}
-                    />
-
-                    <Route path="/addAd" element={<AddAd/>}/>
-                    <Route path="/:id" element={<OglasDetalj cardData={cardData}/>}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <NavbarElement isLoggedIn={isLoggedIn} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home cardData={cardData} isLoggedIn={isLoggedIn} />}
+          />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/login"
+            element={<Login setLoginStatus={setLoginStatus} />}
+          />
+          <Route path="/addAd" element={<AddAd />} />
+          <Route path="/:id" element={<OglasDetalj cardData={cardData} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- IzvorniKod/frontend/src/components/NavbarElement.tsx
+++ IzvorniKod/frontend/src/components/NavbarElement.tsx
@@ -1,59 +1,54 @@
-import {useState} from "react";
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./NavbarElement.css";
 
-function NavbarElement({isLoggedIn, userData}) {
-    const [isBrandHovered, setIsBrandHovered] = useState(false);
+function NavbarElement({ isLoggedIn }) {
+  const [isBrandHovered, setIsBrandHovered] = useState(false);
 
-    const handleBrandMouseEnter = () => {
-        setIsBrandHovered(true);
-    };
+  const handleBrandMouseEnter = () => {
+    setIsBrandHovered(true);
+  };
 
-    const handleBrandMouseLeave = () => {
-        setIsBrandHovered(false);
-    };
+  const handleBrandMouseLeave = () => {
+    setIsBrandHovered(false);
+  };
 
-    return (
-        <Navbar expand="lg">
-            <Container>
-                <Navbar.Brand
-                    href="/"
-                    onMouseEnter={handleBrandMouseEnter}
-                    onMouseLeave={handleBrandMouseLeave}
-                    style={{
-                        fontWeight: isBrandHovered ? "bold" : "normal",
-                        transition: "font-weight 0.1s",
-                    }}
-                    className="d-flex align-items-center"
-                >
-                    <img
-                        src="src/icons/logo.png"
-                        alt="logo"
-                        width="20"
-                        height="20"
-                        style={{marginRight: "6px"}}
-                        className="d-inline-block align-top"
-                    />
-                    {""}
-                    Nestali ljubimci
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        {!isLoggedIn && <Nav.Link href="/login">Prijava</Nav.Link>}
-                        {!isLoggedIn && <Nav.Link href="/register">Registracija</Nav.Link>}
-                        {isLoggedIn && (
-                            <Nav.Link disabled className="text-dark fs-4 fw-bold">
-                                Pozdrav, {userData.firstName}! 👋
-                            </Nav.Link>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand
+          href="/"
+          onMouseEnter={handleBrandMouseEnter}
+          onMouseLeave={handleBrandMouseLeave}
+          style={{
+            fontWeight: isBrandHovered ? "bold" : "normal",
+            transition: "font-weight 0.1s",
+          }}
+          className="d-flex align-items-center"
+        >
+          <img
+            src="src/icons/logo.png"
+            alt="logo"
+            width="20"
+            height="20"
+            style={{ marginRight: "6px" }}
+            className="d-inline-block align-top"
+          />
+          {""}
+          Nestali ljubimci
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {!isLoggedIn && <Nav.Link href="/login">Prijava</Nav.Link>}
+            {!isLoggedIn && <Nav.Link href="/register">Registracija</Nav.Link>}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavbarElement;
