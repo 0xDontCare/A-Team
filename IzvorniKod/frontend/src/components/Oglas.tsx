d39b5415be267c6074075d8cb22393bd4ae441a9
Patrik Marinić <patrik.marinic1@gmail.com>
2023-11-17 15:42:36 +0100

    working on design

--- IzvorniKod/frontend/src/components/Oglas.tsx
+++ IzvorniKod/frontend/src/components/Oglas.tsx
@@ -1,55 +1,24 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Oglas.css";
 
 interface CardProps {
-    id: number;
-    title: string;
-    content: string;
+  id: number;
+  title: string;
+  content: string;
 }
 
-const Oglas: React.FC<CardProps> = ({id, title}) => {
-    const [isHovered, setIsHovered] = useState(false);
-
-    const cardStyle = {
-        border: `2px solid ${isHovered ? "black" : "#ccc"}`,
-        borderRadius: "8px",
-        margin: "10px",
-        padding: "10px",
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-        backgroundColor: "#fff",
-        transition: "border 0.3s",
-    };
-
-    const titleStyle = {
-        fontSize: "1.2rem",
-        fontWeight: "bold",
-        marginBottom: "8px",
-    };
-
-    const contentStyle = {
-        fontSize: "1rem",
-        color: "#555",
-    };
-
-    return (
-        <Link
-            to={`/${id}`}
-            style={{textDecoration: "none", color: "inherit"}}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-        >
-            <div className="card" style={cardStyle}>
-                <div className="card-body">
-                    <h5 className="card-title" style={titleStyle}>
-                        Oglas {id}
-                    </h5>
-                    <p className="card-text" style={contentStyle}>
-                        {title}
-                    </p>
-                </div>
-            </div>
-        </Link>
-    );
+const Oglas: React.FC<CardProps> = ({ id, title }) => {
+  return (
+    <Link to={`/${id}`}>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Oglas {id}</h5>
+          <p className="card-text">{title}</p>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default Oglas;
