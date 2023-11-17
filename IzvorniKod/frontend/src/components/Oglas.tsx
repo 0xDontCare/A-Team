648fd2cf60d43bd640ed8940bcf4daf5deffa0c8
Patrik Marinić <patrik.marinic1@gmail.com>
2023-11-17 15:47:06 +0100

    working on design

--- IzvorniKod/frontend/src/components/Oglas.tsx
+++ IzvorniKod/frontend/src/components/Oglas.tsx
@@ -11,10 +11,10 @@ interface CardProps {
 const Oglas: React.FC<CardProps> = ({ id, title }) => {
   return (
     <Link to={`/${id}`}>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">Oglas {id}</h5>
-          <p className="card-text">{title}</p>
+      <div className="card cardContainer">
+        <div className="card-body cardBody">
+          <h5 className="card-title cardTitle">Oglas {id}</h5>
+          <p className="card-text cardText">{title}</p>
         </div>
       </div>
     </Link>
