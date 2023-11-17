d9003e140e1599be7adfcd296d60f7072e622409
0xDontCare <hogar1.veliki@gmail.com>
2023-11-17 22:43:37 +0100

    Ciscenje datoteka
    Ciscenje datoteka koje su nakon merge procesa zavrsile na krivom mjestu

--- IzvorniKod/backend/src/main/java/com/lostpetfinder/exception/FileUploadFailedException.java
+++
@@ -1,9 +0,0 @@
-package com.lostpetfinder.exception;
-
-public class FileUploadFailedException extends RuntimeException {
-
-    public FileUploadFailedException(String message) {
-        super(message);
-    }
-
-}
--- IzvorniKod/backend/src/main/java/com/lostpetfinder/exception/ImageNotSelectedException.java
+++
@@ -1,8 +0,0 @@
-package com.lostpetfinder.exception;
-
-public class ImageNotSelectedException extends RuntimeException {
-
-    public ImageNotSelectedException(String message) {
-        super(message);
-    }
-}
