cfab3f857b088e1dcf5ed0edb0a20c95fe63acf3
Patrik Marinić <patrik.marinic1@gmail.com>
2023-11-17 15:04:53 +0100

    working on design

--- IzvorniKod/frontend/src/App.tsx
+++ IzvorniKod/frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
@@ -9,50 +9,55 @@ import AddAd from "./components/AddAd";
 import axios from "axios";
 
 function App() {
-    const [isLoggedIn, setLoginStatus] = useState(false);
-    const [userData, setUserData] = useState(null);
+  const [isLoggedIn, setLoginStatus] = useState(false);
+  const [userData, setUserData] = useState(null);
 
-    useEffect(() => {
-        if (isLoggedIn) {
-            axios.get("/api/logged")
-                .then((response) => {
-                    setUserData(response.data);
-                })
-                .catch((error) => {
-                    console.error("Error fetching user data: ", error);
-                });
-        }
-    }, [isLoggedIn]);
+  useEffect(() => {
+    if (isLoggedIn) {
+      axios
+        .get("/api/logged")
+        .then((response) => {
+          setUserData(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching user data: ", error);
+        });
+    }
+  }, [isLoggedIn]);
 
-    const prijaviUser = () => {
-        setLoginStatus(true);
-    };
+  const prijaviUser = () => {
+    setLoginStatus(true);
+  };
 
-    const odjaviUser = () => {
-        setLoginStatus(false);
-    };
+  const odjaviUser = () => {
+    setLoginStatus(false);
+  };
 
-    return (
-        <div>
-            <BrowserRouter>
-                <NavbarElement isLoggedIn={isLoggedIn} userData={userData} setLoginStatus={odjaviUser}/>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<Home isLoggedIn={isLoggedIn}/>}
-                    />
-                    <Route path="/register" element={<Register/>}/>
-                    <Route
-                        path="/login"
-                        element={<Login setLoginStatus={prijaviUser} setUserData={setUserData}/>}
-                    />
+  return (
+    <div>
+      <BrowserRouter>
+        <NavbarElement
+          isLoggedIn={isLoggedIn}
+          userData={userData}
+          setLoginStatus={odjaviUser}
+        />
+        <Routes>
+          <Route path="/" element={<Home isLoggedIn={true} />} />
+          {/* vrati isloggedin=isLoggedIn */}
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/login"
+            element={
+              <Login setLoginStatus={prijaviUser} setUserData={setUserData} />
+            }
+          />
 
-                    <Route path="/addAd" element={<AddAd/>}/>
-                    <Route path="/:id" element={<OglasDetalj/>}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+          <Route path="/addAd" element={<AddAd />} />
+          <Route path="/:id" element={<OglasDetalj />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
