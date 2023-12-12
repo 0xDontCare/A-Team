15116fb01885f517e8b6ea4a833d5f61ca216102
andrijakrklec <andrija.krklec@fer.hr>
2023-12-12 16:49:27 +0100

    izmijenjena konfiguracija za lokalno testiranje

--- IzvorniKod/frontend/vite.config.ts
+++ IzvorniKod/frontend/vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-let API_BASE_URL = process.env.API_BASE_URL;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: API_BASE_URL,
+        target: "http://localhost:8080",
         changeOrigin: true,
       },
     },
