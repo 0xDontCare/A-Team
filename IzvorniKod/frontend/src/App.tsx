542ba3b2cf7e89eb39ef630af86a07c6a21566ff
Patrik Marinić <patrik.marinic1@gmail.com>
2024-01-17 17:41:50 +0100

    izradivanje chat-a na front-endu

--- IzvorniKod/frontend/src/App.tsx
+++ IzvorniKod/frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import Registration from "./components/Registration.tsx";
 import Login from "./components/Login";
@@ -10,64 +10,67 @@ import AddAd from "./components/AddAd";
 import ChangeAd from "./components/ChangeAd.tsx";
 
 function App() {
-    const [loginStatus, setLoginStatus] = useState(false);
-    const [userData, setUserData] = useState(null);
+  const [loginStatus, setLoginStatus] = useState(false);
+  const [userData, setUserData] = useState(null);
 
-    useEffect(() => {
-        const checkLoginStatus = async () => {
-            try {
-                const response = await axios.get("/api/logged");
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const response = await axios.get("/api/logged");
 
-                if (response.data) {
-                    setLoginStatus(true);
-                    setUserData(response.data);
-                } else {
-                    setLoginStatus(false);
-                }
-            } catch (error) {
-                if (error.response && error.response.status === 400) {
-                    setLoginStatus(false);
-                } else {
-                    console.error("Error fetching user data: ", error);
-                }
+        if (response.data) {
+          setLoginStatus(true);
+          setUserData(response.data);
+        } else {
+          setLoginStatus(false);
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 400) {
+          setLoginStatus(false);
+        } else {
+          console.error("Error fetching user data: ", error);
+        }
+      }
+    };
+
+    void checkLoginStatus();
+  }, []);
+
+  const loginUser = () => {
+    setLoginStatus(true);
+  };
+
+  const logoutUser = () => {
+    setLoginStatus(false);
+  };
+
+  return (
+    <div>
+      <BrowserRouter>
+        <NavbarElement
+          isLoggedIn={loginStatus}
+          userData={userData}
+          setLoginStatus={logoutUser}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home isLoggedIn={loginStatus} userData={userData} />}
+          />
+          <Route path="/register" element={<Registration />} />
+          <Route
+            path="/login"
+            element={
+              <Login setLoginStatus={loginUser} setUserData={setUserData} />
             }
-        };
-
-        void checkLoginStatus();
-    }, []);
-
-    const loginUser = () => {
-        setLoginStatus(true);
-    };
-
-    const logoutUser = () => {
-        setLoginStatus(false);
-    };
-
-    return (
-        <div>
-            <BrowserRouter>
-                <NavbarElement
-                    isLoggedIn={loginStatus}
-                    userData={userData}
-                    setLoginStatus={logoutUser}
-                />
-                <Routes>
-                    <Route path="/" element={<Home isLoggedIn={loginStatus} userData={userData}/>}/>
-                    <Route path="/register" element={<Registration/>}/>
-                    <Route
-                        path="/login"
-                        element={
-                            <Login setLoginStatus={loginUser} setUserData={setUserData}/>
-                        }
-                    />
-                    <Route path="/addAd" element={<AddAd userData={userData}/>}/>
-                    <Route path="/changeAd/:id" element={<ChangeAd/>}/>
-                    <Route path="/:id" element={<AdDetail/>}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+          />
+          <Route path="/addAd" element={<AddAd userData={userData} />} />
+          <Route path="/changeAd/:id" element={<ChangeAd />} />
+          <Route path="/:id" element={<AdDetail loginStatus={loginStatus} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
