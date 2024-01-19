5f86623c1ad171f5b6faee8e211591607076519d
akrklec1411 <akrklec@hotmail.com>
2024-01-19 18:00:09 +0100

    promjene kod poziva maps api-ja

--- IzvorniKod/backend/src/main/java/com/lostpetfinder/service/LocationService.java
+++ IzvorniKod/backend/src/main/java/com/lostpetfinder/service/LocationService.java
@@ -8,11 +8,20 @@ import com.lostpetfinder.dto.MapsSummaryDTO;
 import com.lostpetfinder.entity.County;
 import com.lostpetfinder.entity.Location;
 import com.lostpetfinder.entity.Place;
-import org.springframework.http.HttpEntity;
+import org.asynchttpclient.AsyncHttpClientState;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 
 @Service
 public class LocationService {
@@ -32,24 +41,59 @@ public class LocationService {
 
     protected MapsSummaryDTO getLocInfoFromAPI(double latitude, double longitude) {
 
-        RestTemplate restTemplate = new RestTemplate();
-        String apiKey = "AIzaSyADeDhchhmc_T9sPGCLzfFplOeg9nqrrzQ";
+//        RestTemplate restTemplate = new RestTemplate();
+        String apiKey = "AIzaSyDXFHTxz_VlUm8TRSq9D_6xsiIuLiUf3vs";
 
         int radius = 10000;
 
-        HttpEntity<String> entity = new HttpEntity<>(null, null);
-        ResponseEntity<MapsApiResponseDTO> mapsApiResponse = restTemplate.exchange(
-                "https://maps.googleapis.com/maps/api/geocode/json?key=" +
-                        apiKey +
-                        "&latlng=" +
-                        latitude +
-                        "," +
-                        longitude +
-                        "&radius=" + radius +
-                        "&language=hr",
-                HttpMethod.GET, entity, MapsApiResponseDTO.class);
+//        ResponseEntity<MapsApiResponseDTO> mapsApiResponse = restTemplate.exchange(
+//                "https://maps.googleapis.com/maps/api/geocode/json?key=" +
+//                        apiKey +
+//                        "&latlng=" +
+//                        latitude +
+//                        "," +
+//                        longitude +
+//                        "&radius=" + radius +
+//                        "&language=hr",
+//                HttpMethod.GET, null, MapsApiResponseDTO.class);
 
-        return new MapsSummaryDTO(latitude, longitude, mapsApiResponse.getBody());
+        HttpClient httpClient = HttpClients.createDefault();
+
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?key=" +
+                apiKey +
+                "&latlng=" +
+                latitude +
+                "," +
+                longitude +
+                "&radius=" + radius +
+                "&language=hr";
+
+        HttpGet httpGet = new HttpGet(url);
+
+        MapsApiResponseDTO mapsApiResponseDTO = null;
+        try {
+            // Execute the GET request
+            HttpResponse response = httpClient.execute(httpGet);
+
+            // Get the response code
+            //int statusCode = response.getStatusLine().getStatusCode();
+            //System.out.println("Response Code: " + statusCode);
+
+            // Get the response entity
+            HttpEntity entity = response.getEntity();
+
+            // Read the response content
+            if (entity != null) {
+                String responseBody = EntityUtils.toString(entity);
+                ObjectMapper objectMapper = new ObjectMapper();
+                mapsApiResponseDTO = objectMapper.readValue(responseBody, MapsApiResponseDTO.class);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new MapsSummaryDTO(latitude, longitude, mapsApiResponseDTO);
 
     }
 
