f9f328c7b020981bb754fbc25baffcbbed5cf89c
0xDontCare <hogar1.veliki@gmail.com>
2024-01-20 00:36:26 +0100

    Revert "Merge branch 'dev' of https://github.com/0xDontCare/A-Team into devlocal"
    This reverts commit 08b0b58fcf1c6996809cb81f95495d0acfaa505a, reversing
    changes made to 119e1ff5010e64c464ffd91ddcf0cc63dcc67ca9.

--- IzvorniKod/frontend/src/components/ChatRoom.tsx
+++ IzvorniKod/frontend/src/components/ChatRoom.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef} from "react";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import { Button, Card } from "react-bootstrap";
-
 import "./ChatRoom.css";
 import "leaflet/dist/leaflet.css";
 
@@ -97,6 +96,7 @@ function ChatRoom({ advertisementId, loginStatus, userData }: ChatRoomProps) {
       messageText: newMessage.messageText,
       disappearanceLocationLat: newMessage.disappearanceLocationLat,
       disappearanceLocationLng: newMessage.disappearanceLocationLng,
+      images: [newMessage.images],
       linkToImage: newMessage.linkToImage,
     };
 
